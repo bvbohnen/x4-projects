c4deab82870a887fdf0d1e3a0fa5814771ca8ebf
Brent <bvbohnen@ucdavis.edu>
2019-07-23 02:29:20 -0700

    Initial upload.  API basically functional in initial tests, though pending development for robustness.

---
+++ Python_Server/Test.py
@@ -0,0 +1,165 @@
+'''
+Named pipe testing.
+'''
+# This has an example of serving and clienting a pipe
+# https://stackoverflow.com/questions/48542644/python-and-windows-named-pipes
+#
+# This discuesses when to create, connect, disconnect, close.
+#  In short, create at start, connect/disconnect as needed, close at end.
+# https://stackoverflow.com/questions/18077145/windows-named-pipes-in-practice
+
+# Can see if a pipe server is open using this in the windows powershell:
+# [System.IO.Directory]::GetFiles("\\.\\pipe\\")
+
+# Note: import pywin32 as win32api, if needed, though subpackages are
+# directly available.
+import win32pipe
+import win32file
+import threading
+
+'''
+Note: the lua side winapi uses this for opening the pipe:
+  HANDLE hPipe = CreateFile(
+      pipename,
+      GENERIC_READ |  // read and write access
+      GENERIC_WRITE,
+      0,              // no sharing
+      NULL,           // default security attributes
+      OPEN_EXISTING,  // opens existing pipe
+      0,              // default attributes
+      NULL);          // no template file
+'''
+# Name of the pipe in the OS.
+# Name must be: "//<server>/pipe/<pipename>"
+pipe_name = r"\\.\pipe\x4_pipe"
+
+
+def Runtime_Test(pure_python = False):
+    '''
+    Fleshing out this test to be more thorough.
+    The interface will use a memory style protocol, where 
+
+    * pure_python
+      - Bool, if True then the test is done purely in python, with a local
+        client accessing the pipe.
+      - Default is False, leaving the pipe open for x4 to connect to.
+    '''
+    # Can either have x4 open it and python listen in, or python open
+    # it and x4 listen. Or have either open it?  Unclear on how these work.
+    # At any rate, try to open it.
+    pipe = win32pipe.CreateNamedPipe(
+        # Note: for some dumb reason, this doesn't use keyword args,
+        #  so arg names included in comments.
+        # pipeName
+        pipe_name, 
+        # The lua winapi opens pipes as read/write; try to match that.
+        # openMode
+        win32pipe.PIPE_ACCESS_DUPLEX,
+        # pipeMode
+        # Set writes to message, reads to message.
+        # This means reading from the pipe grabs a complete message
+        # as written, instead of a lump of bytes.
+        win32pipe.PIPE_TYPE_MESSAGE | win32pipe.PIPE_READMODE_MESSAGE | win32pipe.PIPE_WAIT,
+        # nMaxInstances
+        1, 
+        # nOutBufferSize
+        65536, 
+        # nInBufferSize
+        65536,
+        # nDefaultTimeOut
+        300,
+        # sa
+        None)
+    print('Started serving: '+pipe_name)
+
+    # For python testing, kick off a client thread.
+    if pure_python:
+        # Set up the reader in another thread.
+        reader_thread = threading.Thread(target = Pipe_Client_Test)
+        reader_thread.start()
+
+    # Set up connections.
+    # This appears to be a stall op that waits for a client to connect.
+    # Returns 0, an integer for okayish errors (io pending, or pipe already
+    #  connected), or raises an exception on other errors.
+    # If the client connected first, don't consider that an error, so
+    #  just ignore any error code but let exceptions get raised.
+    win32pipe.ConnectNamedPipe(pipe, None)
+    print('Connected to client')
+
+
+    # X4 will write to its output, read from its input.
+    # Loop over the expected number of test requests.
+    # These will be read/write transactions to a data table, stored here.
+    test_count = 2
+    data_store = {}
+    for _ in range(test_count):
+
+        # Get the next control message.
+        error, data = win32file.ReadFile(pipe, 64*1024)
+        message = data.decode()
+        print('Received: ' + message)
+
+        # Handle based on prefix, write or read.
+        if message.startswith('write:'):
+            # Expected format is:
+            #  write:[key]value
+            # (Or possibly a chain of keys? just one for now)
+            key, value = message.split(':')[1].split(']')
+            # Pull out the starting bracket.
+            key = key[1:]
+            # Save.
+            data_store[key] = value
+
+        elif message.startswith('read:'):
+            # Expected format is:
+            #  read:[key]
+            key = message.split(':')[1]
+            key = key[1:-1]
+
+            if key not in data_store:
+                response = 'error: {} not found'.format(key)
+            else:
+                response = data_store[key]
+
+            # Pipe the response back.
+            # Note: data is binary in the pipe, but treated as string in lua,
+            # plua lua apparently has no integers (just doubles), but does have
+            # string pack/unpack functions.
+            # Anyway, it is easiest to make strings canonical for this, for now.
+            # TODO: consider passing some sort of format specifier so the
+            #  data can be recast in the lua.
+            error2, bytes_written = win32file.WriteFile(pipe, str(response).encode('utf-8'))
+            print('Returned: ' + response)
+
+        else:
+            print('Unexpected message type')
+
+    
+    # Close the pipe.
+    # The routine for closing is described here:
+    # https://docs.microsoft.com/en-us/windows/win32/ipc/named-pipe-operations
+    win32file.FlushFileBuffers(pipe)
+    win32pipe.DisconnectNamedPipe(pipe)
+    win32file.CloseHandle(pipe)
+    return
+
+
+def Pipe_Client_Test():
+    '''
+    Function to attach to the pipe and read from it.
+    Used to test basic pipe win32api.
+    '''
+    # Send a plain write, then a read transaction.
+    pipe = open(pipe_name, 'rb+', buffering=0)
+    pipe.write(b'write:[test1]5')
+    pipe.write(b'read:[test1]')
+    message = pipe.read(65536)
+    print('Client read: ' + message.decode())
+    
+    return
+
+
+
+if __name__ == '__main__':
+    Runtime_Test(pure_python = False)
---
+++ Python_Server/__init__.py
@@ -0,0 +1,19 @@
+'''
+X4 runtime pipe server.
+This will aim to handle communication with the game: sending commands and
+reading back responses.
+
+Initial version is mostly for testing stuff.
+'''
+
+#-Removed linkup to the x4 customizer for git release.
+'''
+# Set up the path to the customizer framework.
+def _Init():
+    import sys
+    from pathlib import Path
+    parent_dir = Path('.').resolve().parents[1]
+    if str(parent_dir) not in sys.path:
+        sys.path.append(str(parent_dir))
+_Init()
+'''
