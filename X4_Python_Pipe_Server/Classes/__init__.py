7d23f11b3b7dc307a3f3d76b74539099878c6107
Brent <bvbohnen@ucdavis.edu>
2019-10-22 21:48:37 -0700

    Removing files for projects split into separate repos, in prep for submodule linking to them. Leaving just doc/release support here.

--- X4_Python_Pipe_Server/Classes/__init__.py
+++
@@ -1,5 +0,0 @@
-'''
-Support classes for the python server.
-'''
-from .Server_Thread import Server_Thread
-from .Pipe import Pipe_Server, Pipe_Client
--- X4_Python_Pipe_Server/Servers/__init__.py
+++
@@ -1,6 +0,0 @@
-'''
-Basic servers, used for testing.
-TODO: remove all this, fully move over to having the test
-in the extension folder.
-'''
-from . import Test1
