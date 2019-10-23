37e8d35b54362db8a2b04aad12b6dcc1dfae91ef
Brent <bvbohnen@ucdavis.edu>
2019-10-23 11:35:21 -0700

    Removing files split into a separate repo.

--- X4_Files/__init__.py
+++
@@ -1,7 +0,0 @@
-'''
-This folder contains the named_pipes_api extension, to be dropped into
-(or symlinked to) the X4 Foundations/extensions folder.
-
-Currently has no python component.
-TODO: Make_Release, which will pack files into a zip.
-'''
