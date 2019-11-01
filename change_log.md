8efaf8883fbbfd27e24ae4ef92fa0a3e4e55ad58
Brent <bvbohnen@ucdavis.edu>
2019-11-01 00:15:32 -0700

    Various testing, fixed a couple bugs.

--- change_log.md
+++ change_log.md
@@ -3,3 +3,5 @@ Change Log
 
 * 0.1
   - Initial version of lua api.
+* 0.2
+  - Various debugging.
--- time_api/Time_API.lua
+++ time_api/Time_API.lua
@@ -7,10 +7,11 @@ The normal MD timing is paused in such cases, since it operates
 on game time.
 
 Makes use of the GetCurRealTime() function, available globally, which
-appears to return a time in seconds (probably since epoch or similar).
+appears to return a time in seconds that the game has been active since
+loading. (Tabbed-out time not included.)
 
-TODO: maybe regular clock support, but low priority since users can
-create one easily enough.
+Note: preliminary testing suggests GetCurRealTime may only update once
+each frame, and won't be suitable for in-frame code performance testing.
 ]]
 
 --[[ @doc-functions
@@ -25,7 +26,9 @@ each command will take an <id> unique string parameter.
 Commands:
   - getRealTime (<id>)
     - Returns the current real time in seconds, as a long float.
-    - Likely this is time since the epoch, as defined by local OS.
+    - Note: this is the number of seconds since the game was loaded, counting
+      only time while the game has been active (eg. ignores time while
+      minimized).
     - Capture the time using event_ui_triggered.
   - startTimer (<id>)
     - Starts a timer instance under <id>.
@@ -63,12 +66,12 @@ Commands:
       <event_cue_signalled/>
     </conditions>
     <actions>
-      <raise_lua_event name="'Time.getRealTime'"  param="'my_test'"/>
+      <raise_lua_event name="'Time.getRealTime'" param="'my_test'"/>
     </actions>
     <cues>
       <cue name="Capture" instantiate="true">
         <conditions>
-          <event_ui_triggered screen="'Time'" control="'my_test'" /> />
+          <event_ui_triggered screen="'Time'" control="'my_test'" />
         </conditions>
         <actions>
           <set_value name="$realtime" exact="event.param3"/>
@@ -90,7 +93,7 @@ Commands:
     <cues>
       <cue name="Wakeup" instantiate="true">
         <conditions>
-          <event_ui_triggered screen="'Time'" control="'my_alarm'" /> />
+          <event_ui_triggered screen="'Time'" control="'my_alarm'" />
         </conditions>
         <actions>
           <.../>
@@ -103,6 +106,7 @@ Commands:
 
 -- Table of local functions and data.
 local loc = {
+    debug = false,
     -- Flag, if true then alarms are being checked every cycle.
     checking_alarms = false,
     }
@@ -134,7 +138,7 @@ function loc.Raise_Signal(name, return_value)
     AddUITriggeredEvent("Time", name, return_value)
 end
 
-function loc.Get_RealTime(id)
+function loc.Get_RealTime(_, id)
     local now = GetCurRealTime()
     loc.Raise_Signal(id, now)
 end
@@ -156,12 +160,12 @@ end
 function loc.Update_Timer(id)
     local now = GetCurRealTime()
     if loc.timers[id].running then
-        loc.timers[id].total = now - loc.timers[id].last_check
+        loc.timers[id].total = loc.timers[id].total + now - loc.timers[id].last_check
     end
     loc.timers[id].last_check = now
 end
 
-function loc.Start_Timer(id)
+function loc.Start_Timer(_, id)
     loc.Make_Timer(id)
     -- Update it, since it may already be running, and needs to initialize
     -- its last_check anyway.
@@ -169,21 +173,21 @@ function loc.Start_Timer(id)
     loc.timers[id].running = true
 end
 
-function loc.Stop_Timer(id)
+function loc.Stop_Timer(_, id)
     loc.Make_Timer(id)
     -- Update it, to capture any time up to this stop.
     loc.Update_Timer(id)
     loc.timers[id].running = false
 end
 
-function loc.Get_Timer(id)
+function loc.Get_Timer(_, id)
     loc.Make_Timer(id)
     -- Make sure it is up to date if still running.
     loc.Update_Timer(id)
     loc.Raise_Signal(id, loc.timers[id].total)
 end
 
-function loc.Reset_Timer(id)
+function loc.Reset_Timer(_, id)
     loc.Make_Timer(id)
     -- Update it, mostly to reset the last_check.
     loc.Update_Timer(id)
@@ -191,7 +195,7 @@ function loc.Reset_Timer(id)
     loc.timers[id].total = 0
 end
 
-function loc.Print_Timer(id)
+function loc.Print_Timer(_, id)
     loc.Make_Timer(id)
     loc.Update_Timer(id)
     -- Put the time in the log.
@@ -200,23 +204,23 @@ function loc.Print_Timer(id)
         id, loc.timers[id].total))
 end
 
-function loc.Tic(id)
+function loc.Tic(_, id)
     -- Use the timer system, giving a probably unique name.
     local name = "TicToc_"..id
     -- Reset it, which also makes it if needed.
-    loc.Reset_Timer(name)
+    loc.Reset_Timer(_, name)
     -- Start it off.
-    loc.Stop_Timer(name)
+    loc.Start_Timer(_, name)
     -- TODO: maybe optimize the above to not double-update.
 end
 
 
-function loc.Toc(id)
+function loc.Toc(_, id)
     -- Use the timer system, giving a probably unique name.
     local name = "TicToc_"..id
 
     -- Safety in case toc called without tic.
-    loc.Make_Timer(id)
+    loc.Make_Timer(name)
     -- Get it updated.
     loc.Update_Timer(name)
 
@@ -231,7 +235,10 @@ function loc.Toc(id)
 end
 
 
-function loc.Set_Alarm(id_time)
+function loc.Set_Alarm(_, id_time)
+    if debug then
+        DebugError("Time.Set_Alarm got: "..tostring(id_time))
+    end
 
     -- Split the input to separate id and time.
     local id, time = loc.Split_String(id_time)
@@ -245,6 +252,10 @@ function loc.Set_Alarm(id_time)
     -- Start polling if not already.
     if not loc.checking_alarms then
         SetScript("onUpdate", loc.Poll_For_Alarm)
+        loc.checking_alarms = true
+        if debug then
+            DebugError("Time.Set_Alarm started polling")
+        end
     end    
 end
 
@@ -284,6 +295,10 @@ function loc.Poll_For_Alarm()
     -- If no alarms remaining, stop polling.
     if not alarms_still_pending then
         RemoveScript("onUpdate", loc.Poll_For_Alarm)
+        loc.checking_alarms = false
+        if debug then
+            DebugError("Time.Set_Alarm stopped polling")
+        end
     end
 end
 
@@ -296,7 +311,7 @@ function loc.Split_String(this_string)
     -- Get the position of the separator.
     local position = string.find(this_string, ":")
     if position == nil then
-        error("Bad separator")
+        error("Bad separator in: "..tostring(this_string))
     end
     -- Split into pre- and post- separator strings.
     local left  = string.sub(this_string, 0, position -1)
